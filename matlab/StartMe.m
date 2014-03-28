4ad3cedecae11b4e4a6c8768e633a5e76b00d00a
Gang Shu <gxs213@case.edu>
2014-03-27 20:47:24 -0400

    minor change

--- matlab/StartMe.m
+++ matlab/StartMe.m
@@ -1,16 +1,18 @@
-function varargout = EDF_View(edfName, edfPath, xmlName, xmlPath)
+function EDF_View(edfName, edfPath, xmlName, xmlPath)
 
     global EdfFilePath;
     global EdfFileName;
     global XmlFilePath;
     global XmlFileName;
-    %EdfFilePath = 'D:\ABC\';
-    %EdfFileName = 'ABC_012345.edf';
-    %XmlFilePath = 'D:\ABC\';
-    %XmlFileName = 'ABC_012345_MIMI.xml';
     EdfFilePath = edfName;
     EdfFileName = edfPath;
     XmlFilePath = xmlName;
     XmlFileName = xmlPath;
     
+    %EdfFilePath = 'D:\ABC\';
+    %EdfFileName = 'ABC_012345.edf';
+    %XmlFilePath = 'D:\ABC\';
+    %XmlFileName = 'ABC_012345_MIMI.xml';
+    
     EDF_View({})
+end
