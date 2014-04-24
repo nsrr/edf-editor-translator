c4db16146305da26ee01cd59352a052251a6c7be
Gang Shu <gxs213@case.edu>
2014-04-24 19:58:23 -0400

    Cleanup to source code and document of EDF-Editor-Translator

--- matlab/StartMe_dialogPars.m
+++
@@ -1,7 +0,0 @@
-function StartMe_dialogPars(edfName, edfPath, xmlName, xmlPath)
-    
-    global needOpenDialog;
-    needOpenDialog = logical(1);
-    
-    EDF_View({})
-end
--- matlab/StartMe_examplePars.m
+++
@@ -1,4 +0,0 @@
-function StartMe_examplePars()
-    StartMe_cmdPars('C:\Study Files\ABC\', 'ABC_012345.edf', 'C:\Study Files\ABC\', 'ABC_012345_MIMI.xml');
-    %StartMe_cmdPars('D:\Study Files\ABC\', 'ABC_012345.edf', '', '');
-end
